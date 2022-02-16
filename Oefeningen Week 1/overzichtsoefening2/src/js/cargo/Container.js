a79dec32d02bf8acf1e6146d0454a3a8b1cc3d99
Yasin El Bannasri <11801187@student.pxl.be>
2022-02-16 20:23:55 +0100

    added getProducts and getProductAtIndex

--- Oefeningen Week 1/overzichtsoefening2/src/js/cargo/Container.js
+++ Oefeningen Week 1/overzichtsoefening2/src/js/cargo/Container.js
@@ -2,9 +2,27 @@
 // naam:
 import Product from './Product';
 export default class Container{
-    constructor() {
-        this._products = [];
-
+    constructor(maxWeight) {
+        if (maxWeight < 0) {
+            throw new Error('Maxweight under 0');
+        } else {
+            this._products = [];
+            const _maxWeight = maxWeight;
+        }
     }
+    addProduct(product) {
+        if (product instanceof Product) {
+            this._products.push(product);
+        } else {
+            throw 'Not instance of product'
+        }
+    }
+    getProductAtIndex(index) {
+        if (index >= 0 && typeof index == 'number' && index < this._products.length) {
+            
+        } else {
+            throw new Error('Index out of bounds')
+        }
+    } 
 
 }
